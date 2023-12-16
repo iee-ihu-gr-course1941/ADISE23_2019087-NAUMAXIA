9d5f8015959383b2ac8a8878dfa0137ac5d84969
kindi24 <kyndelerosvasilis@gmail.com>
2023-12-16 22:42:46 +0200

    Initial commit

--- Game/logincheck.js
+++ Game/logincheck.js
@@ -14,8 +14,6 @@ function login() {
         success: function (response) {
             if (response === "success") {
                 alert("Επιτυχής σύνδεση!");	
-                var x = '../Game/game.html'
-                window.location.assign(x);
             } else {
                 alert("Ο χρήστης δεν υπάρχει ή δόθηκαν λάθος στοιχεία.");
             }
