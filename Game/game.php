8ef585c0a8807478c48720c501d47e56dfaafe46
kindi24 <kyndelerosvasilis@gmail.com>
2024-01-04 20:34:29 +0200

    Update game.php

--- Game/game.php
+++ Game/game.php
@@ -1,18 +1,18 @@
 <?php
 function show_status() {
 	
-	global $mysqli;
+	    global $mysqli;
 	
-	check_abort_status();
+	    check_abort_status();
 	
-	$sql = 'SELECT * FROM game_status';
-	$st = $mysqli->prepare($sql);
+	    $sql = 'SELECT * FROM game_status';
+	    $st = $mysqli->prepare($sql);
 
-	$st->execute();
-	$res = $st->get_result();
+	    $st->execute();
+	    $res = $st->get_result();
 
-	header('Content-type: application/json');
-	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+	    header('Content-type: application/json');
+	    print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
 
     }
 
